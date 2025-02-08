0da1cf037cbac836c449a6a4a9be8a42f6733aa7
christopher-moeller <35485851+christopher-moeller@users.noreply.github.com>
2025-02-08 11:21:11 +0100

    premake refactoring

---
+++ GATE/premake5.lua
@@ -0,0 +1,38 @@
+project "GATE"
+    location "GATE"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    { 
+        "src/**.h", 
+        "src/**.cpp"
+    }
+
+    includedirs
+    {
+        "src",
+    }
+
+    filter "system:macosx"
+        defines { "GATE_PLATFORM_MACOS" }
+
+    filter "system:windows"
+        defines { "GATE_PLATFORM_WINDOWS" }
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines { "GATE_DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "GATE_RELEASE" }
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines { "GATE_DIST" }
+        optimize "On"
---
+++ MacSandbox/premake5.lua
@@ -0,0 +1,48 @@
+project "MacSandbox"
+    location "MacSandbox"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    dependson { "GATE" } -- Ensure GATE builds first
+
+    files 
+    { 
+        "src/**.h", 
+        "src/**.cpp"
+    }
+
+    includedirs
+    {
+        "../GATE/src"
+    }
+
+    libdirs { 
+        "bin/" .. outputdir .. "/GATE"
+     }
+    links
+    {
+        "GATE",
+    }
+
+    filter "system:macosx"
+        defines { "GATE_PLATFORM_MACOS" }
+
+    filter "system:windows"
+        defines { "GATE_PLATFORM_WINDOWS" }
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines { "GATE_DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "GATE_RELEASE" }
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines { "GATE_DIST" }
+        optimize "On"
